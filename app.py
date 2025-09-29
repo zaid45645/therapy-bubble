d285bcf57e869d409a86795aaedab83e81e79c4b
Mohammed Zaid <70617604+zaid45645@users.noreply.github.com>
2025-09-29 22:07:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 import requests
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, session
 
 app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return render_template('index.html')
 
 @app.route("/chat", methods=['POST'])
 def chat():
@@ -13,15 +9,24 @@ def chat():
     url = "https://ai.hackclub.com/chat/completions"
     headers = {"Content-Type": "application/json"}
     data = {
-        "messages": [
-            {"role": "user", "content": f"dont mention <think> and </think> in the prompt and keep it short talk like a therapist: {user_message}"}
-        ]
-    }
+    "messages": [
+        {
+            "role": "user",
+            "content": f"Reply as a therapist to: {user_message}. Use a friendly and supportive tone and keep it short",
+        }
+    ],
+    "reasoning_format": "parsed"
+}
     response = requests.post(url, json=data, headers=headers)
     ai_response = response.json()
     ai_message = ai_response.get("choices", [{}])[0].get("message", {}).get("content")
     print(ai_message)
     return jsonify({"ai_response": ai_message})
+
+@app.route("/")
+def hello():
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
